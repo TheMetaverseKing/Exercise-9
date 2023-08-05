81c0ed708d2f74a2ee5f57be01272c061adc65b5
TheMetaverseKing <139680450+TheMetaverseKing@users.noreply.github.com>
2023-08-05 12:48:26 +0500

    Add files via upload

---
+++ eX9.js
@@ -0,0 +1,5 @@
+"use strict";
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+let favouriatenumber = 7981;
+let message = "This message reveals my favouriate number, which is ";
+console.log(message + favouriatenumber);
---
+++ eX9.ts
@@ -0,0 +1,5 @@
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+let favouriatenumber : number = 7981;
+let message:string = "This message reveals my favouriate number, which is ";
+console.log(message + favouriatenumber);
